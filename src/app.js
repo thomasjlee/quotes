205a8a0e6ac76087b9997dd3fe51e55f637f2ef8
Thomas Lee <leethomasjae@gmail.com>
2019-04-03 20:52:25 -0500

    fetch quotes on mount

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
+const QUOTES_URL = 'https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json';
+
 new Vue({
   el: '#app',
   data: {
-    
+    quotes: []
+  },
+  mounted: function() {
+    fetch(QUOTES_URL)
+      .then(response => response.json())
+      .then(quotes => this.quotes = quotes)
+      .catch(err => console.error(err));
   }
-})
+});
